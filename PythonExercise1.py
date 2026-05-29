9c4862926bfcea1a3d178f6169ea58350756f51e
Peter Dennison <pd290@bath.ac.uk>
2026-05-29 08:46:07 +0100

    Python Exercise 1

--- PythonExercise1.py
+++ PythonExercise1.py
@@ -6,7 +6,15 @@ Question 1:
 '''
  
 def printHelloWorld():
-  print("Hello, world!");
+  x = 9  # Replace with any integer of your choice
+  print("The original number is " + str(x))
+  newVar = x + 3
+  newVar *= 2
+  newVar -= 4
+  newVar -= 2 * x
+  newVar += 3
+  print("The result is " + str(newVar))
+
 
 '''
 Question 2: 
@@ -17,7 +25,17 @@ Question 2:
 '''
 
 def printXTenTimes():
-  print(str(3));
+  print(str(3))
+  print(str(3+1))
+  print(str(3+2))
+  print(str(3+3))
+  print(str(3+4))
+  print(str(3+5))
+  print(str(3+6))
+  print(str(3+7))
+  print(str(3+8))
+  print(str(3+9))
+  print(str(3+10))
 
 '''
 Question 3: 
@@ -35,8 +53,13 @@ Question 3:
  124 cm is ? feet ? inches to the nearest inch.
 '''
 
+
 def convertMetricToImperialHeights():
-  print(str(101) + " is 3 feet 3 inches to the nearest inch.");
+  x = 192
+  ft = int(x/30.54)
+  cm = int((x-ft*30.54)/2.54)
+  print(str(101) + " is 3 feet 3 inches to the nearest inch.")
+  print(str(x) + " cm is " + str(int(ft)) + " feet " + str(int(cm)) + " inches to the nearest inch.")
 
 '''
 Question 4: 
@@ -51,14 +74,24 @@ Question 4:
 '''
 
 def fibonacci():
-  print(str(2));
+  previous = 0
+  current = 1
+  print(str(previous))
+  print(str(current))
+  for i in range(0,8):
+    next = previous + current
+    previous = current
+    current = next
+    print(str(current))
 
 '''
  Question 5: 
  
- Complete the function below so that it uses two variables: height and radius. Use these two variables and print to the screen, the volume of a cylinder. 
+ Complete the function below so that it uses two variables: height and radius. 
+ Use these two variables and print to the screen, the volume of a cylinder. 
 
- Call this function from the main to test your program.  For example, you could test your program with the following values, 
+ Call this function from the main to test your program.  
+ For example, you could test your program with the following values, 
 
  height 7.0cm and radius 4.0cm
  height 20.0cm and radius 3.0cm
@@ -70,8 +103,12 @@ def fibonacci():
 '''
 
 def volumeOfACylinder():
-  print("the cylinder with height 7.0cm and radius 4.0cm has a volume of 351.86cm^3")
-
+  height = (14.7)
+  radius = (5.2)
+  volume = 3.14159 * (radius * radius) * height
+  print("the cylinder with height " + str(height) + "cm and radius " + str(radius) + "cm has a volume of " + str(round(volume, 2)) + "cm^3")
+  print("the cylinder with height ", str(height), "cm and radius ", str(radius), "cm has a volume of ", str(round(volume, 2)), "cm^3")
+  print(f"the cylinder with height {height}cm and radius {radius}cm has a volume of {round(volume, 2)}cm^3")
 
 print("Question 1\n");
 printHelloWorld();
